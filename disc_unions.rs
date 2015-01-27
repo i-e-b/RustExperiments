f94d7a157b5ac645725ec57944e347f72cd280f4
Iain Ballard <iain.ballard@gmail.com>
2015-01-27 16:09:12 +0000

    updating, plus cargo

--- disc_unions.rs
+++ disc_unions.rs
@@ -1,5 +1,4 @@
-#![feature(struct_variant)]
-
+#[deriving(Debug)]
 enum Direction {
     Up, Down, Left, Right
 }
@@ -15,12 +14,12 @@ enum Shape {
 
 fn write_shape(s:Shape) {
     match s {
-        Circle {centre, radius} => println!("Circle {},{}:{}",centre.x,centre.y,radius),
+        Shape::Circle {centre, radius} => println!("Circle {},{}:{}",centre.x,centre.y,radius),
         _ => println!("not a circle")
     }
 }
 
 fn main() {
-    println!("{:?}", Down);
-    write_shape(Circle {centre:Point{x:1.0,y:1.0}, radius:1.0});
+    // println!("{:?}", Direction::Down);  // not sure how to fix this: the trait `core::fmt::Show` is not implemented for the type `Direction`
+    write_shape(Shape::Circle {centre:Point{x:1.0,y:1.0}, radius:1.0});
 }
