ac10f7790154a6b31002fbf006346bb7e03bba86
Iain Ballard <iain.ballard@gmail.com>
2016-06-09 13:50:17 +0100

    guessing game

--- guessing_game/README.md
+++ guessing_game/README.md
@@ -1,7 +1,8 @@
 Example from http://doc.rust-lang.org/book/guessing-game.html
 
 To build: `cargo build` in this directory.
-To run, `cargo run`
+To run, `cargo run`. This will also do a build if the sources have changed.
 
 The file `Cargo.toml` is the build definition.
+The `Cargo.lock` file is a shrink-wrap of the versions and sources used in the package.
 
--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,12 +1,40 @@
+extern crate rand;
+
 use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
 
 fn main(){
-    println!("Guess the number!");
-    println!("Please input your guess:");
+    let secret_number = rand::thread_rng().gen_range(1, 101);
 
-    let mut guess = String::new();
+    println!("Guess the number, between 1 and 100");
 
-    io::stdin().read_line(&mut guess).expect("Failed to read input");
+    loop { // infinite loop without silly tricks :-)
+        println!("Please input your guess: ");
 
-    println!("You guessed:{}", guess);
+        let mut guess = String::new();
+        io::stdin() // from standard input,
+            .read_line(&mut guess) // read up to newline, changing the guess value and returning io::Result
+            .expect("Failed to read input"); // handle io::Result failure by crashing the program.
+
+        let guess: u32 = match guess.trim().parse() { // shadow the old guess with a new numeric one
+            Ok(num) => num,
+            Err(_)  => {
+                println!("Please enter a number");
+                continue;
+            }
+        }; // the semicolon here is needed to end the assignment of `guess`
+
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less    => println!("Too small"),
+            Ordering::Greater => println!("Too big"),
+            Ordering::Equal   => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
+
